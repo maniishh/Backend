c073421ba4b3cdd117bc56c420027594e4d0a360
Manish Kumar Yadav <162919290+maniishh@users.noreply.github.com>
2026-03-03 22:16:51 +0530

    jwt

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -51,5 +51,26 @@ userSchema.pre("save",async function(next){
     this.password=await bcrypt.hash(this.password,10)
     next()
 })
+userSchema.methods.isPasswordCorrect=async function(password){
+    return await bcrypt.compare(password,this.password)//returns true or false
+}
 
+userSchema.methods.generateAccessToken=function(){
+    return jwt.sign({
+        _id:this._id,
+        email:this.email,
+        username:this.username,
+        fullname:this.fullname
+
+    },
+    process.env.ACCESS_TOKEN_SECRET,{
+        expiresIn:process.env.ACESS_TOKEN_EXPIRE_TIME
+    })
+}
+userSchema.methods.generateRefreshToken=function(){
+    return jwt.sign({
+        _id:this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,{expiresIn:process.env.REFRESH_TOKEN_EXPIRE_TIME})
+}
 export const User=mongoose.model("User",userSchema)
