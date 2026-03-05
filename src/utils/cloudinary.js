d56e44f44b8b80758ae1b7a5cdb7044e23e7c870
Manish Kumar Yadav <162919290+maniishh@users.noreply.github.com>
2026-03-05 15:50:53 +0530

    Update project files and dependencies

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,30 @@
+import {v2 as cloudinary} from 'cloudinary';
+import fs from 'fs';
+
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});  
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try{
+        if(!localFilePath) return null;
+        //upload the file on cludinary
+        const response=await cloudinary.uploader.upload(localFilePath,{
+            resource_type: 'auto',
+        })
+        console.log("file is ulpoaded on cloudinary", response.url);
+        return response;
+
+    }
+    catch(error){
+        fs.unlinkSync(localFilePath);//delete the file from local storage as operation is failed
+        console.log("error while uploading file on cloudinary", error);
+        return null;
+
+    }
+}
+
+export {uploadOnCloudinary};
