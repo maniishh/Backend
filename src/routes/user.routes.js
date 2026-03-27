8a00a8e8e86131d823138e50bbfe72d1670c7d18
Manish Kumar Yadav <162919290+maniishh@users.noreply.github.com>
2026-03-27 07:40:04 +0530

    bugs

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,7 @@
 import {Router} from "express";
-import { logOutUser, registerUser } from "../controllers/user.controller.js";
+import { logOutUser, registerUser, loginUser} from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 
 const router=Router();
