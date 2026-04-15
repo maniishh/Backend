612188fc763929323d24547e51f68be26dd04804
Manish Kumar Yadav <162919290+maniishh@users.noreply.github.com>
2026-04-15 23:20:56 +0530

    feat: add getWatchHistory function to fetch user's watch history with video details

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -372,6 +372,59 @@ const getUserChannelProfile=asyncHandler(async(req,res)=>{
     .json(new ApiResponse(200,channel[0],"Channel profile fetched successfully"))
 
 })
+
+const getWatchHistory=asyncHandler(async(req,res)=>{
+    //get user id from req.user
+    //find user in db and populate watch history with video details
+    //send response
+    const user=await User.aggregate([
+        {
+            $match:{
+                _id:new mongoose.Types.ObjectId(req.user._id)
+            }
+
+        },
+        {
+            $lookup:{
+                from:"videos",
+                localField:"watchHistory",
+                foreignField:"_id",
+                as:"watchHistory",
+                pipeline:[{
+                    $lookup:{
+                        from:"users",
+                        localField:"owner",
+                        foreignField:"_id",
+                        as:"owner",
+                        pipeline:[{
+                            $project:{
+                                fullName:1,
+                                username:1,
+                                avatar:1
+                            }
+                        },
+                        {
+                          $addFields:{
+                            owner:{
+                                $first:"$owner"
+                            }
+                          }  
+                        }
+                    ]
+                    }
+
+                }
+                
+
+                
+                ]
+            }
+        },  
+    ])
+    return res
+    .status(200)
+    .json(new ApiResponse(200,user,"Watch history fetched successfully"))
+})
 export {
     registerUser,
     loginUser,
@@ -382,5 +435,6 @@ export {
     updateAccountDetails,
     updateUserAvatar,
     updateCoverImage,
-    getUserChannelProfile
+    getUserChannelProfile,
+    getWatchHistory
 }
