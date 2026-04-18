02be5f608dbd2136ea76431e984ba489ae5db8d0
Manish Kumar Yadav <162919290+maniishh@users.noreply.github.com>
2026-04-18 20:32:31 +0530

    feat: update mongoose connection options and improve API response structure

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,4 +1,4 @@
-import { asyncHandler } from "../utils/asyncHandler.js";
+import mongoose from "mongoose";import { asyncHandler } from "../utils/asyncHandler.js";
 import {ApiError} from "../utils/ApiError.js";
 import {User}  from "../models/user.model.js";
 import {uploadOnCloudinary} from "../utils/cloudinary.js";
@@ -135,8 +135,8 @@ const logOutUser=asyncHandler(async(req,res)=>{
     //clear cookies and send response
     User.findByIdAndUpdate(req.user._id,
         {
-            $set:{
-                refreshToken:undefined
+            $unset:{
+                refreshToken:1//1 means delete the refreshToken field from the user document    
         }
     },
     {
--- src/db/index.js
+++ src/db/index.js
@@ -6,7 +6,13 @@ import {DB_NAME} from '../constants.js';
 
 const conncetDB=async()=>{
     try {
-      const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+      const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`, {
+        ssl: true,
+        tlsAllowInvalidCertificates: false,
+        tlsAllowInvalidHostnames: false,
+        serverSelectionTimeoutMS: 5000,
+        socketTimeoutMS: 45000,
+      })
         console.log(`\nDB connected successfully: ${connectionInstance.connection.host}\n`);
 
     } catch (error) {
